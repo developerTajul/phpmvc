abbfcb878cf0dfa9b73915d7b92c314e4ad13612
developerTajul <developer.tajul@gmail.com>
2022-08-07 16:36:51 +0600

    fixed

---
+++ app/Controllers/AboutController.php
@@ -0,0 +1,9 @@
+<?php 
+namespace App\Controllers;
+
+class AboutController{
+
+    public function index(){
+        return "This is about page from AboutController";
+    }
+}
--- routes/route.php
+++ routes/route.php
@@ -1,12 +1,16 @@
 <?php 
 use Pecee\SimpleRouter\SimpleRouter;
-use App\Controllers\PortfoliosController;
+use App\Controllers\HomeController;
+use App\Controllers\AboutController;
 
-SimpleRouter::get('phpmvc/', function() {
-    return 'This is Homepage';
-});
+// SimpleRouter::get('phpmvc/', function() {
+//     return 'This is Homepage';
+// });
+
+SimpleRouter::get('phpmvc/', [HomeController::class, 'index']);
+SimpleRouter::get('phpmvc/about', [AboutController::class, 'index']);
 
 
-SimpleRouter::get('phpmvc/portfolio', [PortfoliosController::class, 'index']);
+
 
 
