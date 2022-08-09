764b615955b3aaf63fe21ece849ad10e8f5bd849
developerTajul <developer.tajul@gmail.com>
2022-08-09 23:38:56 +0600

    fixed

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
-
 use Pecee\SimpleRouter\SimpleRouter;
-require_once __DIR__ . './vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
 /* Load external routes file */
 require_once 'routes/route.php';
@@ -15,3 +14,4 @@ SimpleRouter::setDefaultNamespace('\App\Controllers');
 
 // Start the routing
 SimpleRouter::start();
+
